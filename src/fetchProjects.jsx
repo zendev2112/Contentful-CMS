fe7824244e1fa28e819d083639f1cbff64f18d84
zen <zenon.deviagge@gmail.com>
2024-05-03 19:37:33 -0300

    Create client variable using createClient method from contentful. Create `useFetchProjects` custom hook. Set `loading` and `projects` state variables. Setup getData function to fetch `client` object  using getEntries method. Store it in `response` variable. Access the contents of the variable and destructure `title`, `url`, `image` and `id`. Set as properties of `projects` object. Set `projects` object as state variable. Invoke `getData` inside useEffect hook. Export `loading` and `projects` state variables to Projects.jsx component

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { createClient } from 'contentful'
 
 const client = createClient({
@@ -6,6 +7,30 @@ const client = createClient({
   accessToken: 'uVLuxtimD6CMp6J5AWOz8jdFyN4Ln5u7mem5Om_PCMY',
 })
 
-client
-  .getEntries({ content_type: 'projects' })
-  .then((response) => console.log(response))
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' })
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields
+        const id = item.sys.id
+        const img = image?.fields?.file?.url
+        return { title, url, id, img }
+      })
+      setProjects(projects)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  return { loading, projects }
+}
+
